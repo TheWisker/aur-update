fdd6507860c0d18a00815ae573bf5e2eac18a60e
TheWisker <TheWisker@protonmail.com>
2023-07-01 08:59:01 +0200

    Prerelease tests

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,7 +9,5 @@ echo "Host *
     UserKnownHostsFile /dev/null
     " > /root/.ssh/config
 echo "${aur_key}" > /root/.ssh/aur
-echo "${aur_public_key}" > /root/.ssh/aur.pub
 chmod 0700 /root/.ssh/aur
-chmod 0700 /root/.ssh/aur.pub
 echo "Configured SSH Credentials"
